4316829271eb4933db1de59bc5112f041ec9406b
Đặng Văn Ánh <107748343+anhutc@users.noreply.github.com>
2023-06-21 20:55:29 +0700

    Update view.js

--- assets/js/view.js
+++ assets/js/view.js
@@ -1,37 +1,37 @@
 function shuffle(array) {
-    let currentIndex = array.length, randomIndex;
+  let currentIndex = array.length, randomIndex;
 
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
 
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
 
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-
-    return array;
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
   }
 
-  $.getJSON('assets/js/data.json', function (data) {
+  return array;
+}
 
-    datarandom = (shuffle(data))
+$.getJSON('assets/js/data.json', function (data) {
 
-    $('#views').empty().hide();
+  datarandom = (shuffle(data))
 
-    for (let i = 0; i < datarandom.length; i++) {
+  $('#views').empty().hide();
 
-      var html = '<a href="' + datarandom[i].url + '" target="_blank" rel="noopener">';
-      html += '<div class="Card four columns" style="opacity: 1; transform: none;">';
-      html += '<img class="cover" src="' + datarandom[i].img + '" alt="' + datarandom[i].title + '">';
-      html += '<div class="data">';
-      html += '<h2>' + datarandom[i].title + '</h2>';
-      html += '<p>' + datarandom[i].description + '</p></div></div></a>';
+  for (let i = 0; i < datarandom.length; i++) {
 
-      $('#views').append(html).fadeIn();
-    };
+    var html = '<a href="' + datarandom[i].url + '" target="_blank" rel="noopener">';
+    html += '<div class="Card four columns" style="opacity: 1; transform: none;">';
+    html += '<img class="cover" src="' + datarandom[i].img + '" alt="' + datarandom[i].title + '">';
+    html += '<div class="data">';
+    html += '<h2>' + datarandom[i].title + '</h2>';
+    html += '<p>' + datarandom[i].description + '</p></div></div></a>';
 
-  });
+    $('#views').append(html).fadeIn();
+  };
+
+});
