5a8c79ec444d102d0a583e3671a6dfb8b2c083ac
Đặng Văn Ánh <107748343+anhutc@users.noreply.github.com>
2023-08-02 09:31:07 +0700

    a

--- chill/scripts/script.js
+++ chill/scripts/script.js
@@ -14,7 +14,7 @@ new Vue({
           artist: "Norm Ender",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
           source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/1.mp3",
-          url: "https://www.youtube.com/watch?v=z3wAjJXbYzA",
+          url: "https://www.youtube.com/watch?v=Ik2i84IrRoc",
           favorited: false
         },
         {
