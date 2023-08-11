85d8e93fd74bc7432a53b9872cd2d952c5dcf997
Đặng Văn Ánh <107748343+anhutc@users.noreply.github.com>
2023-08-11 23:08:26 +0700

    Update view.js

--- assets/js/view.js
+++ assets/js/view.js
@@ -27,7 +27,7 @@ $.getJSON('assets/js/data.json', function (data) {
 
     var html = '<a href="' + datarandom[i].url + '" target="_blank" rel="noopener">';
     html += '<div class="Card four columns" style="opacity: 1; transform: none;">';
-    html += '<img class="cover" src="' + datarandom[i].img + '" alt="' + datarandom[i].title + '">';
+    html += '<img class="cover" srcset="' + datarandom[i].img + '" alt="' + datarandom[i].title + '">';
     html += '<div class="data">';
     html += '<h2>' + datarandom[i].title + '</h2>';
     html += '<p>' + datarandom[i].description + '</p></div></div></a>';
